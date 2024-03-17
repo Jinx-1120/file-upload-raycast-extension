1fe2d2920683fd1631e5d8face7a524d616b7dbf
jinhaidi <jinhaidi@laiye.com>
2024-03-18 00:43:27 +0800

    v1.0 stable

--- src/index.tsx
+++ src/index.tsx
@@ -1,85 +1,116 @@
-import { ActionPanel, Form, Action, Clipboard, showToast } from "@raycast/api";
-import { useEffect, useState } from "react";
-import { getFinderSelectedImages } from "./util";
-import fs from "fs/promises";
-import path from "path";
-import crypto from "crypto";
-import axios from "axios";
-import FormData from "form-data";
-
-const uploadFile = (fileBuffer: Buffer, fileName: string, ext: string, filePath?: string) => {
-  const formData = new FormData();
-  formData.append("content", fileBuffer, fileName);
-  formData.append("file_name", fileName);
-  if (filePath) {
-    formData.append("file_path", filePath);
-  }
-  formData.append("file_type", `image/${ext}`);
-
-  return axios
-    .post("https://ucm.laiye.com/api/dialogue/upload/file", formData, {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    })
-    .then((res) => {
-      const { data, error } = res.data;
-
-      if (error?.error_code !== 0) {
-        return Promise.reject(error.error_detail);
-      } else {
-        return Promise.resolve(data?.url);
-      }
-    })
-    .catch(Promise.reject);
+import { ActionPanel, Form, Action, Icon, openExtensionPreferences, Detail, LocalStorage } from "@raycast/api";
+import { useState } from "react";
+import { getFile, isImage, uploadFile } from "./common/util";
+import { ImageMeta } from "./common/type";
+import { ImageDetailMetadata } from "./common/image-detail-metadat";
+import { imageMeta } from "image-meta";
+type StateType = {
+  status: "selecting" | "no-input" | "image-format-invalid" | "canceled" | "error" | "succeed";
+  message: string;
+  cache?: boolean;
+  image?: ImageMeta;
 };
 export default function Command() {
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    const loadMedia = async () => {
-      const finder = await getFinderSelectedImages();
-      console.log("finder", finder);
-    };
-    loadMedia();
-  }, []);
+  const [state, setState] = useState<StateType>({
+    status: "selecting",
+    message: "**uploading...**",
+  });
 
-  const submit = async (values: { file: string[]; file_name: string; file_path: string }) => {
-    const { file, file_name, file_path } = values;
-    const file_url = file[0];
-    setLoading(true);
-    const data = await fs.readFile(file_url);
-
-    let name = file_name;
-    const type = path.extname(file_url).slice(1);
-    if (!file_name) {
-      const hash = crypto.createHash("sha256").update(data).digest("hex").substring(0, 15);
-      name = `${hash}.${type}`;
-    }
-    if (!name.includes(type)) {
-      name += `.${type}`;
-    }
-
-    uploadFile(data, name, type, file_path)
-      .then(async (res) => {
-        await Clipboard.copy(res);
-        showToast({ title: "upload success", message: `url copied to clipboard` });
-      })
-      .finally(() => {
-        setLoading(false);
+  const submit = async (values: { file: string[]; file_name: string; sub_path: string }) => {
+    const { file, file_name, sub_path } = values;
+    if (file.length === 0) {
+      setState({
+        status: "error",
+        message: "Input is not an image",
       });
-  };
-  return (
-    <Form
-      isLoading={loading}
-      actions={
-        <ActionPanel>
-          <Action.SubmitForm title="立即上传" onSubmit={submit} />
-        </ActionPanel>
+      return;
+    }
+    const file_url = file[0];
+    const { fileBuffer, fileName, fileExt, hash, type } = await getFile(file_url);
+
+    const record = await LocalStorage.getItem<string>(hash);
+    if (record) {
+      const cacheImg = JSON.parse(record) as ImageMeta;
+      setState({
+        status: "succeed",
+        message: isImage(cacheImg.format) ? `![Image Title](${cacheImg.url})` : `# The resource is not an image`,
+        cache: true,
+        image: cacheImg,
+      });
+      return;
+    } else {
+      const url = await uploadFile(fileBuffer, file_name != "" ? file_name : fileName, fileExt, sub_path);
+      const newRecord: ImageMeta = {
+        hash,
+        from: "selected",
+        source: file_url,
+        format: type,
+        url,
+        size: fileBuffer.length,
+        createdAt: Date.now(),
+      };
+      const isImg = isImage(type);
+      if (isImg) {
+        const meta = await imageMeta(fileBuffer);
+        newRecord.width = meta.width;
+        newRecord.height = meta.height;
       }
-    >
-      <Form.FilePicker id="file" title="choose file" allowMultipleSelection={false} />
-      <Form.TextField id="file_name" title="file name" />
-      <Form.TextField id="file_path" title="sub file path" defaultValue="assets/" placeholder="Should end with ‘/’" />
-    </Form>
-  );
+
+      await LocalStorage.setItem(hash, JSON.stringify(newRecord));
+
+      setState({
+        status: "succeed",
+        cache: false,
+        message: isImg ? `![Image Title](${url})` : "# The resource is not an image",
+        image: newRecord,
+      });
+    }
+  };
+  const navigationTitle = state.status === "error" ? "Failed to upload" : "Uploaded successfully";
+  if (state.status === "selecting") {
+    return (
+      <Form
+        isLoading={true}
+        actions={
+          <ActionPanel>
+            <Action.SubmitForm title="Upload Now" onSubmit={submit} />
+          </ActionPanel>
+        }
+      >
+        <Form.FilePicker id="file" title="choose file" allowMultipleSelection={false} />
+        <Form.TextField id="file_name" title="file name" />
+        <Form.TextField id="sub_path" title="sub file path" defaultValue="assets/" placeholder="Should end with ‘/’" />
+      </Form>
+    );
+  } else {
+    return (
+      <Detail
+        markdown={state.message}
+        navigationTitle={navigationTitle}
+        metadata={state.status === "succeed" ? <ImageDetailMetadata image={state.image!} /> : null}
+        actions={
+          state.status === "succeed" ? (
+            <ActionPanel>
+              <Action.CopyToClipboard
+                icon={Icon.CopyClipboard}
+                title="Copy Image CDN URL to Clipboard"
+                content={state.image!.url}
+              />
+              <Action.OpenInBrowser url={state.image!.url} />
+              <Action.CopyToClipboard
+                icon={Icon.CopyClipboard}
+                title="Copy Markdown Content to Clipboard"
+                content={`![image from clipboard](${state.image!.url})`}
+              />
+              <Action title="Open Extension Preferences" onAction={openExtensionPreferences} />
+            </ActionPanel>
+          ) : state.status === "error" ? (
+            <ActionPanel>
+              <Action title="Open Extension Preferences" onAction={openExtensionPreferences} />
+            </ActionPanel>
+          ) : null
+        }
+      />
+    );
+  }
 }
